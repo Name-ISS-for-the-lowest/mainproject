66a44a8e55bac7f4fe4b1ee5f23b1eb49d6f4aa3
Gabriel Malek <malek.gabriel33@gmail.com>
2023-10-21 21:55:41 -0700

    login page

---
+++ frontend/lib/home.dart
@@ -0,0 +1,193 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class Home extends StatefulWidget {
+  final String title;
+
+  const Home({super.key, required this.title});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        resizeToAvoidBottomInset: false,
+        body: Stack(
+          children: [
+            Positioned.fill(
+              child: Align(
+                alignment: Alignment.bottomCenter,
+                child: Container(
+                    height: MediaQuery.of(context).size.height * 0.6,
+                    child: SvgPicture.asset(
+                      'assets/BackGround.svg',
+                      fit: BoxFit.fitHeight,
+                    )),
+              ),
+            ),
+            LogIn(),
+          ],
+        ));
+  }
+}
+
+class LogIn extends StatefulWidget {
+  const LogIn({super.key});
+
+  @override
+  State<LogIn> createState() => _LogInState();
+}
+
+class _LogInState extends State<LogIn> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        //spacer for top
+        SizedBox(
+          height: 120,
+        ),
+        //Log in title
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Center(
+            child: Text("Log In",
+                style: TextStyle(
+                    fontSize: 50,
+                    fontWeight: FontWeight.w700,
+                    color: Color.fromRGBO(230, 183, 17, 1))),
+          ),
+        ),
+        //Input fields
+        Column(
+          children: [
+            SizedBox(
+              height: 50,
+            ),
+            Column(
+              children: [
+                Column(
+                  children: [
+                    Container(
+                      width: 340,
+                      child: TextField(
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10.0)),
+                          labelText: 'Email',
+                          contentPadding: EdgeInsets.all(18),
+                          fillColor: Colors.white,
+                          filled: true,
+                          floatingLabelBehavior: FloatingLabelBehavior.never,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+                SizedBox(
+                  height: 5,
+                ),
+                Column(
+                  children: [
+                    Container(
+                      width: 340,
+                      child: TextField(
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10.0)),
+                          labelText: 'Password',
+                          contentPadding: EdgeInsets.all(18),
+                          fillColor: Colors.white,
+                          filled: true,
+                          floatingLabelBehavior: FloatingLabelBehavior.never,
+                        ),
+                      ),
+                    )
+                  ],
+                )
+              ],
+            )
+          ],
+        ),
+        Column(
+          children: [
+            Column(
+              children: [
+                SizedBox(
+                  height: 50,
+                ),
+                Column(
+                  children: [
+                    Center(
+                      child: Container(
+                        width: 340,
+                        height: 50,
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Color.fromRGBO(230, 183, 17, 1),
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10.0)),
+                          ),
+                          onPressed: () => {},
+                          child: Text(
+                            "Login",
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w700,
+                              color: Color.fromARGB(255, 53, 53, 53),
+                            ),
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+                SizedBox(
+                  height: 5,
+                ),
+                Column(
+                  children: [
+                    Center(
+                      child: Container(
+                        width: 340,
+                        height: 50,
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Color.fromRGBO(221, 151, 26, 1),
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10.0)),
+                          ),
+                          onPressed: () => {},
+                          child: Text(
+                            "Sign Up",
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w700,
+                              color: Color.fromARGB(255, 53, 53, 53),
+                            ),
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                )
+              ],
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            Text("Forgot Password?",
+                style: TextStyle(
+                  fontSize: 14,
+                  fontWeight: FontWeight.w400,
+                  color: Color.fromRGBO(230, 183, 17, 1),
+                ))
+          ],
+        )
+      ],
+    );
+  }
+}
