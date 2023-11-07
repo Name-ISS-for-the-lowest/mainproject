a838e80ececad2cd942d42ba7f51392785ef1315
Desarso <malek.gabriel33@gmail.com>
2023-11-07 15:05:38 -0800

    fixed port

--- frontend/lib/classes/auth_helper.dart
+++ frontend/lib/classes/auth_helper.dart
@@ -4,7 +4,7 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 class AuthHelper {
-  static String defaultHost = "http://10.0.2.2:5000";
+  static String defaultHost = "http://10.0.2.2:8000";
   static String sessionCookie = "";
 
   static Future<Response> login(String email, String password) async {
