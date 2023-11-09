1e9142c2770ca27e03c3e0e975c0eb93482ad4d5
Dylan Cochran <dylancochran@csus.edu>
2023-11-08 23:22:35 -0800

    Added the bar thing at the top

--- frontend/lib/views/ForumPage.dart
+++ frontend/lib/views/ForumPage.dart
@@ -9,46 +9,116 @@ class ForumPage extends StatefulWidget {
 }
 
 class _ForumPageState extends State<ForumPage> {
+  int selectedIndex = 0;
+  void onItemTapped(int index) {
+    setState(() {
+      selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: Color(0xffece7d5),
+        body: Column(
+          children: [
+            SizedBox(
+              height: 75,
+              width: 75,
+              child: SvgPicture.asset("assets/iss-small.svg"),
+            ),
+            Container(
+              height: 1.0, // Set the desired height of the line
+              color: Color(0x5f000000),
+              margin: EdgeInsets.symmetric(vertical: 0),
+            ),
+          ],
+        ),
         bottomNavigationBar: SizedBox(
           height: 80,
           child: BottomNavigationBar(
-            backgroundColor: Color(0xff08231A),
-            iconSize: 40,
-            type: BottomNavigationBarType.fixed,
-            showSelectedLabels: false,
-            showUnselectedLabels: false,
-            selectedItemColor: Color(0xffFFE89A),
-            unselectedItemColor: Color(0xff89875A),
-            items: <BottomNavigationBarItem>[
-              BottomNavigationBarItem(
-                icon: Container(
-                  width: 40,
-                  height: 40,
-                  child: SvgPicture.asset('assets/icon-home.svg'),
+              backgroundColor: Color(0xff08231A),
+              type: BottomNavigationBarType.fixed,
+              showSelectedLabels: false,
+              showUnselectedLabels: false,
+              selectedItemColor: Color(0xffFFE89A),
+              unselectedItemColor: Color(0xff89875A),
+              items: <BottomNavigationBarItem>[
+                BottomNavigationBarItem(
+                  icon: Container(
+                    width: 40,
+                    height: 40,
+                    child: SvgPicture.asset('assets/icon-home.svg',
+                        color: Color(0xff89875A)),
+                  ),
+                  activeIcon: Container(
+                    width: 40,
+                    height: 40,
+                    child: SvgPicture.asset('assets/icon-home.svg',
+                        color: Color(0xffFFE89A)),
+                  ),
+                  label: '',
                 ),
-                label: '',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.business),
-                label: '',
-                backgroundColor: Color(0xff08231A),
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.school),
-                label: '',
-                backgroundColor: Color(0xff08231A),
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.settings),
-                label: '',
-                backgroundColor: Color(0xff08231A),
-              ),
-            ],
-          ),
+                BottomNavigationBarItem(
+                  icon: Container(
+                    width: 40,
+                    height: 40,
+                    child: SvgPicture.asset('assets/icon-search.svg'),
+                  ),
+                  activeIcon: Container(
+                    width: 40,
+                    height: 40,
+                    child: SvgPicture.asset('assets/icon-search.svg',
+                        color: Color(0xffFFE89A)),
+                  ),
+                  label: '',
+                ),
+                BottomNavigationBarItem(
+                  icon: Container(
+                    width: 40,
+                    height: 40,
+                    child: SvgPicture.asset('assets/icon-createpost.svg'),
+                  ),
+                  activeIcon: Container(
+                    width: 40,
+                    height: 40,
+                    child: SvgPicture.asset('assets/icon-createpost.svg',
+                        color: Color(0xffFFE89A)),
+                  ),
+                  label: '',
+                ),
+                BottomNavigationBarItem(
+                  icon: Container(
+                    width: 40,
+                    height: 40,
+                    child: SvgPicture.asset('assets/icon-resources.svg',
+                        color: Color(0xff89875A)),
+                  ),
+                  activeIcon: Container(
+                    width: 40,
+                    height: 40,
+                    child: SvgPicture.asset('assets/icon-resources.svg',
+                        color: Color(0xffFFE89A)),
+                  ),
+                  label: '',
+                ),
+                BottomNavigationBarItem(
+                  icon: Container(
+                    width: 40,
+                    height: 40,
+                    child: SvgPicture.asset('assets/icon-profile.svg'),
+                  ),
+                  activeIcon: Container(
+                    width: 40,
+                    height: 40,
+                    child: SvgPicture.asset('assets/icon-profile.svg',
+                        color: Color(0xffFFE89A)),
+                  ),
+                  label: '',
+                ),
+              ],
+              onTap: onItemTapped,
+              currentIndex: selectedIndex),
         ));
   }
 }
