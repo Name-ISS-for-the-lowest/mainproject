7814d4f50f13341a3ccbe9e1e0e6d3363da0af4f
Desarso <malek.gabriel33@gmail.com>
2023-11-09 17:38:34 -0800

    much improved verification email

--- frontend/lib/classes/auth_helper.dart
+++ frontend/lib/classes/auth_helper.dart
@@ -1,5 +1,6 @@
 import 'package:http/http.dart';
 import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'dart:convert';
 
@@ -19,6 +20,7 @@ class AuthHelper {
         body: jsonEncode(data));
     if (response.statusCode == 200) {
       AuthHelper.sessionCookie = response.headers['set-cookie']!;
+      AuthHelper.writeCookie('sessionCookie', AuthHelper.sessionCookie);
     }
     return response;
   }
@@ -36,14 +38,28 @@ class AuthHelper {
     return response;
   }
 
+  static readCookie(String key) async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getString(key);
+  }
+
+  static writeCookie(String key, String value) async {
+    final prefs = await SharedPreferences.getInstance();
+    prefs.setString(key, value);
+  }
+
+  static deleteCookie(String key) async {
+    final prefs = await SharedPreferences.getInstance();
+    prefs.remove(key);
+  }
+
   static Future<bool> isLoggedIn() async {
-    String endPoint = '/protected';
-    final url = Uri.parse('$defaultHost$endPoint');
-    final response = await http.get(url, headers: <String, String>{
-      'Content-Type': 'application/json; charset=UTF-8',
-      'Cookie': sessionCookie
-    });
-    if (response.statusCode == 200) {
+    String? cookie = await readCookie('sessionCookie');
+    print(cookie);
+    print("hello there");
+    return false;
+    if (cookie != null) {
+      //also need to check for expiration date
       return true;
     } else {
       return false;
