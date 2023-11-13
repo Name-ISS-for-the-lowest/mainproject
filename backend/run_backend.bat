2b9d120cbe2ffa51a54b487daaf4bb6a40969d36
Rylan Pereira <rylanrpcollege@gmail.com>
2023-11-12 17:32:11 -0800

    test commit

--- backend/run_backend.bat
+++ backend/run_backend.bat
@@ -1,3 +1,3 @@
-Powershell -Command "& {C:\Users\dylan\AppData\Local\MongoDBCompass\MongoDBCompass.exe;venv/Scripts/Activate.ps1; uvicorn server:app --reload;}"
+Powershell -Command "& {C:\Users\rylan\AppData\Local\MongoDBCompass\MongoDBCompass.exe;venv/Scripts/Activate.ps1; uvicorn server:app --reload;}"
 
 pause
