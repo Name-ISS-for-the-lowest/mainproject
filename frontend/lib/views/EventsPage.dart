29d4c295d6e954c6cf95a6698543c5fc113e9c9f
Gabriel Malek <malek.gabriel33@gmail.com>
2023-11-12 21:36:02 -0800

    fixed events

--- frontend/lib/views/EventsPage.dart
+++ frontend/lib/views/EventsPage.dart
@@ -1,5 +1,6 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:frontend/classes/eventHelper.dart';
 import 'package:frontend/classes/routeHandler.dart';
 import 'package:html_unescape/html_unescape.dart';
 import 'package:html/parser.dart' show parse;
@@ -15,13 +16,11 @@ class EventsPage extends StatefulWidget {
 }
 
 class _EventsPageState extends State<EventsPage> {
-  var _events = <Map<String, String>>[];
-
   //init
   @override
   void initState() {
     super.initState();
-    fetchEvents();
+    EventHelper.fetchEvents(setState);
   }
 
   Future<void> _launchURL(Uri url) async {
@@ -32,48 +31,6 @@ class _EventsPageState extends State<EventsPage> {
 
 //I need to move this function to events helper where I can make
 //the results memoized by hand since flutter hooks and memoization absolutelty failed me
-  Future fetchEvents() async {
-    final monthsOfYear = {
-      1: 'JAN',
-      2: 'FEB',
-      3: 'MAR',
-      4: 'APR',
-      5: 'MAY',
-      6: 'JUN',
-      7: 'JUL',
-      8: 'AUG',
-      9: 'SEP',
-      10: 'OCT',
-      11: 'NOV',
-      12: 'DEC',
-    };
-
-    try {
-      var unescape = HtmlUnescape();
-      var url = "https://www.trumba.com/calendars/sacramento-state-events.json";
-      final response = await RouteHandler.dio.get(url);
-      var events = response.data;
-      setState(() {
-        for (var event in events) {
-          DateTime dateTime = DateTime.parse(event['startDateTime']);
-          _events.add({
-            'title': unescape.convert(event['title']),
-            'date': unescape.convert(event['dateTimeFormatted']),
-            'location': parse(event['location']).body!.text,
-            'day': dateTime.day.toString(),
-            'month': monthsOfYear[dateTime.month]!,
-            'url': event['permaLinkUrl'],
-          });
-        }
-      });
-      return response;
-    } on DioException catch (e) {
-      print(e);
-      // Handle the error, you might want to throw an exception or return a default response
-      throw e;
-    }
-  }
-
   Widget _buildList() {
     return ListView.builder(
       padding: const EdgeInsets.all(16.0),
@@ -82,16 +39,16 @@ class _EventsPageState extends State<EventsPage> {
 
         final index = item ~/ 2;
         //if at bottom of list show loading indicator
-        if (index == _events.length) {
+        if (index == EventHelper.events.length) {
           return const Center(
             child: CircularProgressIndicator(),
           );
           //right now the events are only 50 long so I return null
           //I am thinking of using a different api to get way more events will see later
-        } else if (index >= _events.length) {
+        } else if (index >= EventHelper.events.length) {
           return null;
         }
-        return _buildEvent(_events[index]);
+        return _buildEvent(EventHelper.events[index]);
       },
     );
   }
