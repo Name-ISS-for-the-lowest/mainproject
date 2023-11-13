721019e2155761494d67a215a9586448a70134da
Gabriel Malek <malek.gabriel33@gmail.com>
2023-11-12 21:41:10 -0800

    set state bug

--- frontend/lib/classes/eventHelper.dart
+++ frontend/lib/classes/eventHelper.dart
@@ -30,6 +30,9 @@ class EventHelper {
       var url = "https://www.trumba.com/calendars/sacramento-state-events.json";
       final response = await RouteHandler.dio.get(url);
       var results = response.data;
+
+      //I need to fix the set state bug, if you click to another page while events are fetching,
+      //an error will be thrown because set state is called on an non-existent widget, RIP.
       setState(() {
         for (var event in results) {
           DateTime dateTime = DateTime.parse(event['startDateTime']);
