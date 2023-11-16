89356f7549a2d2a8409254572fbf3bf5ef4481c7
nazarpot <nazarpot@gmail.com>
2023-11-15 17:44:20 -0800

    basic layout

--- frontend/lib/views/CreatePost.dart
+++ frontend/lib/views/CreatePost.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:frontend/classes/post_helper.dart';
+import 'package:frontend/classes/postHelper.dart';
 import 'package:frontend/views/CoreTemplate.dart';
 
 class CreatePost extends StatefulWidget {
--- frontend/lib/views/ResourceCenter.dart
+++ frontend/lib/views/ResourceCenter.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class ResourceCenter extends StatefulWidget {
   const ResourceCenter({super.key});
@@ -8,11 +9,49 @@ class ResourceCenter extends StatefulWidget {
 }
 
 class _ResourceCenterState extends State<ResourceCenter> {
-  @override
+  /*@override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xffece7d5),
       body: Center(child: Text("Resource Center")),
     );
+  }*/
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Text("Student Resources", style: DefaultTextStyle.of(context).style.apply(fontSizeFactor: 2.0)),
+        Spacer(),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Spacer(),
+            SizedBox(
+            child: SvgPicture.asset("assets/icon-homehome.svg"),
+            width: 75,
+            height: 75,
+            ),
+            Spacer(),
+            SizedBox(
+            child: SvgPicture.asset("assets/icon-visavisa.svg"),
+            width: 75,
+            height: 75,
+            ),
+            Spacer(),
+            SizedBox(
+            child: SvgPicture.asset("assets/icon-jobjob.svg"),
+            width: 75,
+            height: 75,
+            ),
+            Spacer(),
+          ],
+        ),
+        Spacer(),
+      ]
+    );
   }
+  
 }
