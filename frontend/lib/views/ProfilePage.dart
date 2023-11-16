252f0dc455a31add0ee993a85878a7948d708ed2
Gabriel Malek <malek.gabriel33@gmail.com>
2023-11-15 22:56:33 -0800

    minor fixes

--- frontend/lib/views/ProfilePage.dart
+++ frontend/lib/views/ProfilePage.dart
@@ -10,114 +10,89 @@ class ProfilePage extends StatefulWidget {
 class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
-
-    String profilePicture = "assets/pfp-mrwhiskers.png";
+    String profilePicture = "assets/DefaultPFPs/pfp-mrwhiskers.png";
     String displayName = "Mr. Whiskers";
     String nationality = "Brazilian";
     String emailAddress = "mrwhiskers@csus.edu";
 
-
     return Scaffold(
-      backgroundColor: Color(0xffece7d5),
-      // body: Center(child: Text("Profile Page")),
+        backgroundColor: Color(0xffece7d5),
+        // body: Center(child: Text("Profile Page")),
 
-      body: ListView(
-        children: [
-
-          Stack(
-            
-
-            children: <Widget>[
-
-              Positioned(
-                child: Align(
-                  alignment: Alignment.center,
-                  child: Image.asset(
-                  profilePicture,
-                  height: 150,
-                  width: 150,
+        body: ListView(
+          children: [
+            Stack(
+              children: <Widget>[
+                Positioned(
+                  child: Align(
+                    alignment: Alignment.center,
+                    child: Image.asset(
+                      profilePicture,
+                      height: 150,
+                      width: 150,
+                    ),
+                  ),
                 ),
+                Positioned.fill(
+                  left: 140,
+                  child: Align(
+                      alignment: Alignment.bottomCenter,
+                      child: const Icon(
+                        Icons.camera_alt,
+                        size: 50,
+                      )),
                 ),
+              ],
+            ),
+
+            // Container(
+            //   child: Image.asset(
+            //     profilePicture,
+            //     height: 150,
+            //     width: 150,
+            //     //trailing: Icon(Icons.camera_alt),
+            //   ),
+
+            // ),
+
+            //USERNAME
+            Container(
+              child: ListTile(
+                leading: Icon(Icons.person),
+                title: Text('Display Name'),
+                subtitle: Text(displayName),
+                trailing: Icon(Icons.edit_note),
               ),
-
-
-
-              Positioned.fill(
-                left: 140,
-                child: Align(
-                  alignment: Alignment.bottomCenter,
-                  child: const Icon(
-                    Icons.camera_alt,
-                    size: 50,
-                    )
-
-                ),
-                ),
-            ],
-          ),
-
-
-          // Container(
-          //   child: Image.asset(
-          //     profilePicture,
-          //     height: 150,
-          //     width: 150,
-          //     //trailing: Icon(Icons.camera_alt),
-          //   ),
-            
-          // ),
-
-
-
-          //USERNAME
-          Container( 
-            child: ListTile(
-              leading: Icon(Icons.person),
-              title: Text('Display Name'),
-              subtitle: Text(displayName),
-              trailing: Icon(Icons.edit_note),
             ),
-          ),
 
-
-
-          //NATIONALITY
-          Container(
-            child: ListTile(
-              leading: Icon(Icons.flag),
-              title: Text('Nationality'),
-              subtitle: Text(nationality),
-              trailing: Icon(Icons.edit_note),
+            //NATIONALITY
+            Container(
+              child: ListTile(
+                leading: Icon(Icons.flag),
+                title: Text('Nationality'),
+                subtitle: Text(nationality),
+                trailing: Icon(Icons.edit_note),
+              ),
             ),
-          ),
 
-
-          Container(
-            child: ListTile(
-              leading: Icon(Icons.mail),
-              title: Text('Email Address'),
-              subtitle: Text(emailAddress),
+            Container(
+              child: ListTile(
+                leading: Icon(Icons.mail),
+                title: Text('Email Address'),
+                subtitle: Text(emailAddress),
+              ),
             ),
-          ),
 
-          Container(
-            child: ListTile(
-              leading: Icon(Icons.delete),
-              iconColor: Colors.redAccent,
-              title: Text('Delete Account'),
-              subtitle: Text('This action cannot be restored.'),
-              textColor: Colors.redAccent,
+            Container(
+              child: ListTile(
+                leading: Icon(Icons.delete),
+                iconColor: Colors.redAccent,
+                title: Text('Delete Account'),
+                subtitle: Text('This action cannot be restored.'),
+                textColor: Colors.redAccent,
+              ),
             ),
-          ),
-
-
-
-        ],
-      )
-
-
-
-
-    );
+          ],
+        ));
   }
 }
