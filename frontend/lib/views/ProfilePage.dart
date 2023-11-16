01e985f77dffe587f0dc1975d59352dea404c1d3
Rylan Pereira <rylanrpcollege@gmail.com>
2023-11-15 16:59:52 -0800

    pfp layout and generic bat

--- frontend/lib/views/ProfilePage.dart
+++ frontend/lib/views/ProfilePage.dart
@@ -10,9 +10,81 @@ class ProfilePage extends StatefulWidget {
 class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
+
+    String profilePicture = "assets/pfp-mrwhiskers.png";
+    String displayName = "Mr. Whiskers";
+    String nationality = "Brazilian";
+    String emailAddress = "mrwhiskers@csus.edu";
+
+
     return Scaffold(
       backgroundColor: Color(0xffece7d5),
-      body: Center(child: Text("Profile Page")),
+      // body: Center(child: Text("Profile Page")),
+
+      body: ListView(
+        children: [
+
+          Container(
+            child: Image.asset(
+              profilePicture,
+              height: 150,
+              width: 150,
+              //trailing: Icon(Icons.camera_alt),
+            ),
+            
+          ),
+
+
+
+          //USERNAME
+          Container( 
+            child: ListTile(
+              leading: Icon(Icons.person),
+              title: Text('Display Name'),
+              subtitle: Text(displayName),
+              trailing: Icon(Icons.edit_note),
+            ),
+          ),
+
+
+
+          //NATIONALITY
+          Container(
+            child: ListTile(
+              leading: Icon(Icons.flag),
+              title: Text('Nationality'),
+              subtitle: Text(nationality),
+              trailing: Icon(Icons.edit_note),
+            ),
+          ),
+
+
+          Container(
+            child: ListTile(
+              leading: Icon(Icons.mail),
+              title: Text('Email Address'),
+              subtitle: Text(emailAddress),
+            ),
+          ),
+
+          Container(
+            child: ListTile(
+              leading: Icon(Icons.delete),
+              iconColor: Colors.redAccent,
+              title: Text('Delete Account'),
+              subtitle: Text('This action cannot be restored.'),
+              textColor: Colors.redAccent,
+            ),
+          ),
+
+
+
+        ],
+      )
+
+
+
+
     );
   }
 }
