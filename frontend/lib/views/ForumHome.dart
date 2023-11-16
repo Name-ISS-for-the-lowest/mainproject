c35fae75af0319d9f3aa1f48d280b12a948a3fe8
tjudtmartine <145167310+tjudtmartine@users.noreply.github.com>
2023-11-16 15:37:32 -0800

    Update

--- frontend/lib/views/ForumHome.dart
+++ frontend/lib/views/ForumHome.dart
@@ -2,10 +2,6 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-<<<<<<< HEAD
-=======
-import 'package:frontend/classes/authHelper.dart';
->>>>>>> 843f2816411831cd2e62691b6dad661bb1a2fdad
 import 'package:frontend/classes/postHelper.dart';
 
 class ForumHome extends StatefulWidget {
@@ -293,8 +289,4 @@ class _ForumHomeState extends State<ForumHome> {
       ),
     );
   }
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 843f2816411831cd2e62691b6dad661bb1a2fdad
