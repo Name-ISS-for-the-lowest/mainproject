c779c65d6b2f87836431d30cfc044e67906ed9b7
Dylan Cochran <dylancochran@csus.edu>
2023-11-17 17:59:31 -0800

    Did a lot of merges and added translated resource center links

--- frontend/lib/views/ResourceCenter.dart
+++ frontend/lib/views/ResourceCenter.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'package:frontend/classes/authHelper.dart';
 
 class ResourceCenter extends StatefulWidget {
   const ResourceCenter({super.key});
@@ -10,7 +11,7 @@ class ResourceCenter extends StatefulWidget {
 }
 
 class _ResourceCenterState extends State<ResourceCenter> {
-  Future<void> _launchURL(Uri url) async {  
+  Future<void> _launchURL(Uri url) async {
     if (!await launchUrl(url, mode: LaunchMode.externalApplication)) {
       throw Exception('Could not launch $url');
     }
@@ -18,246 +19,248 @@ class _ResourceCenterState extends State<ResourceCenter> {
 
   @override
   Widget build(BuildContext context) {
+    String userLang = AuthHelper.userInfoCache['language'];
     return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        Text("Student Resources", style: DefaultTextStyle.of(context).style.apply(fontSizeFactor: 2.0)),
-        Spacer(),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Spacer(),
-            Container(
-              width: 100,
-              height: 100,
-              decoration: BoxDecoration(
-                color: Colors.black,
-                border: Border.all(
-                  color: Color(0x0008231A)
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text("Student Resources",
+              style: DefaultTextStyle.of(context)
+                  .style
+                  .apply(fontSizeFactor: 2.0)),
+          Spacer(),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Spacer(),
+              Container(
+                width: 100,
+                height: 100,
+                decoration: BoxDecoration(
+                  color: Colors.black,
+                  border: Border.all(color: Color(0x0008231A)),
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
                 ),
-                borderRadius: BorderRadius.all(Radius.circular(10)),
-              ),
-              child: Column(
-                children: [
+                child: Column(children: [
                   Text("Housing", style: TextStyle(color: Colors.white)),
                   IconButton(
                     onPressed: () {
-                      _launchURL(Uri.parse("https://www.csus.edu/international-programs-global-engagement/international-student-scholar-services/housing.html"));}, 
-                    icon: SvgPicture.asset("assets/ResourceCenter/icon-homehome.svg"),
+                      _launchURL(Uri.parse(
+                          "https://www-csus-edu.translate.goog/international-programs-global-engagement/international-student-scholar-services/housing.html?_x_tr_sl=auto&_x_tr_tl=${userLang}}&_x_tr_hl=en&_x_tr_pto=wapp"));
+                    },
+                    icon: SvgPicture.asset(
+                        "assets/ResourceCenter/icon-homehome.svg"),
                     color: Colors.white,
                   ),
-                ]
+                ]),
               ),
-            ),
-            Spacer(),
-            Container(
-              width: 100,
-              height: 100,
-              decoration: BoxDecoration(
-                color: Colors.black,
-                border: Border.all(
-                  color: Color(0x0008231A)
+              Spacer(),
+              Container(
+                width: 100,
+                height: 100,
+                decoration: BoxDecoration(
+                  color: Colors.black,
+                  border: Border.all(color: Color(0x0008231A)),
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
                 ),
-                borderRadius: BorderRadius.all(Radius.circular(10)),
-              ),
-              child: Column(
-                children: [
+                child: Column(children: [
                   Text("F1/J1 Status", style: TextStyle(color: Colors.white)),
                   IconButton(
-                    onPressed: () {_launchURL(Uri.parse("https://www.csus.edu/international-programs-global-engagement/international-student-scholar-services/maintaining-status.html")); }, 
-                    icon: SvgPicture.asset("assets/ResourceCenter/icon-visavisa.svg"),
+                    onPressed: () {
+                      _launchURL(Uri.parse(
+                          "https://www-csus-edu.translate.goog/international-programs-global-engagement/international-student-scholar-services/maintaining-status.html?_x_tr_sl=auto&_x_tr_tl=${userLang}&_x_tr_hl=en&_x_tr_pto=wapp"));
+                    },
+                    icon: SvgPicture.asset(
+                        "assets/ResourceCenter/icon-visavisa.svg"),
                     color: Colors.white,
                   ),
-                ]
+                ]),
               ),
-            ),
-            Spacer(),
-            Container(
-              width: 100,
-              height: 100,
-              decoration: BoxDecoration(
-                color: Colors.black,
-                border: Border.all(
-                  color: Color(0x0008231A)
+              Spacer(),
+              Container(
+                width: 100,
+                height: 100,
+                decoration: BoxDecoration(
+                  color: Colors.black,
+                  border: Border.all(color: Color(0x0008231A)),
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
                 ),
-                borderRadius: BorderRadius.all(Radius.circular(10)),
-              ),
-              child: Column(
-                children: [
+                child: Column(children: [
                   Text("F1/J1 Jobs", style: TextStyle(color: Colors.white)),
                   IconButton(
-                    onPressed: () {_launchURL(Uri.parse("https://www.csus.edu/international-programs-global-engagement/international-student-scholar-services/employment.html")); }, 
-                    icon: SvgPicture.asset("assets/ResourceCenter/icon-jobjob.svg"),
+                    onPressed: () {
+                      _launchURL(Uri.parse(
+                          "https://www-csus-edu.translate.goog/international-programs-global-engagement/international-student-scholar-services/employment.html?_x_tr_sl=auto&_x_tr_tl=${userLang}&_x_tr_hl=en&_x_tr_pto=wapp"));
+                    },
+                    icon: SvgPicture.asset(
+                        "assets/ResourceCenter/icon-jobjob.svg"),
                     color: Colors.white,
                   ),
-                ]
+                ]),
               ),
-            ),
-            Spacer(),
-          ],
-        ),
-        Spacer(),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Spacer(),
-            Container(
-              width: 100,
-              height: 100,
-              decoration: BoxDecoration(
-                color: Colors.black,
-                border: Border.all(
-                  color: Color(0x0008231A)
+              Spacer(),
+            ],
+          ),
+          Spacer(),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Spacer(),
+              Container(
+                width: 100,
+                height: 100,
+                decoration: BoxDecoration(
+                  color: Colors.black,
+                  border: Border.all(color: Color(0x0008231A)),
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
                 ),
-                borderRadius: BorderRadius.all(Radius.circular(10)),
-              ),
-              child: Column(
-                children: [
+                child: Column(children: [
                   Text("Research", style: TextStyle(color: Colors.white)),
                   Text("Scholar", style: TextStyle(color: Colors.white)),
                   IconButton(
-                    onPressed: () {_launchURL(Uri.parse("https://www.csus.edu/international-programs-global-engagement/international-student-scholar-services/international-visiting-research-scholars.html")); }, 
-                    icon: SvgPicture.asset("assets/ResourceCenter/icon-research.svg"),
+                    onPressed: () {
+                      _launchURL(Uri.parse(
+                          "https://www-csus-edu.translate.goog/international-programs-global-engagement/international-student-scholar-services/international-visiting-research-scholars.html?_x_tr_sl=auto&_x_tr_tl=${userLang}&_x_tr_hl=en&_x_tr_pto=wapp"));
+                    },
+                    icon: SvgPicture.asset(
+                        "assets/ResourceCenter/icon-research.svg"),
                     color: Colors.white,
                   ),
-                ]
+                ]),
               ),
-            ),
-            /*SizedBox(
+              /*SizedBox(
             child: SvgPicture.asset("assets/ResourceCenter/icon-research.svg"),
             width: 75,
             height: 75,
             ),*/
-            Spacer(),
-            Container(
-              width: 100,
-              height: 100,
-              decoration: BoxDecoration(
-                color: Colors.black,
-                border: Border.all(
-                  color: Color(0x0008231A)
+              Spacer(),
+              Container(
+                width: 100,
+                height: 100,
+                decoration: BoxDecoration(
+                  color: Colors.black,
+                  border: Border.all(color: Color(0x0008231A)),
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
                 ),
-                borderRadius: BorderRadius.all(Radius.circular(10)),
-              ),
-              child: Column(
-                children: [
+                child: Column(children: [
                   Text("Multicultural", style: TextStyle(color: Colors.white)),
                   Text("Center", style: TextStyle(color: Colors.white)),
                   IconButton(
-                    onPressed: () {_launchURL(Uri.parse("https://www.csus.edu/student-affairs/centers-programs/multi-cultural-center/")); }, 
-                    icon: SvgPicture.asset("assets/ResourceCenter/icon-earthearth.svg"),
+                    onPressed: () {
+                      _launchURL(Uri.parse(
+                          "https://www-csus-edu.translate.goog/student-affairs/centers-programs/multi-cultural-center/?_x_tr_sl=auto&_x_tr_tl=${userLang}&_x_tr_hl=en&_x_tr_pto=wapp"));
+                    },
+                    icon: SvgPicture.asset(
+                        "assets/ResourceCenter/icon-earthearth.svg"),
                     color: Colors.white,
                   ),
-                ]
+                ]),
               ),
-            ),
-            Spacer(),
-            Container(
-              width: 100,
-              height: 100,
-              decoration: BoxDecoration(
-                color: Colors.black,
-                border: Border.all(
-                  color: Color(0x0008231A)
+              Spacer(),
+              Container(
+                width: 100,
+                height: 100,
+                decoration: BoxDecoration(
+                  color: Colors.black,
+                  border: Border.all(color: Color(0x0008231A)),
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
                 ),
-                borderRadius: BorderRadius.all(Radius.circular(10)),
-              ),
-              child: Column(
-                children: [
+                child: Column(children: [
                   Text("Serna Center", style: TextStyle(color: Colors.white)),
                   Spacer(),
                   IconButton(
-                    onPressed: () {_launchURL(Uri.parse("https://www.csus.edu/student-affairs/centers-programs/serna-center/")); }, 
-                    icon: SvgPicture.asset("assets/ResourceCenter/icon-serna.svg"),
+                    onPressed: () {
+                      _launchURL(Uri.parse(
+                          "https://www-csus-edu.translate.goog/student-affairs/centers-programs/serna-center/?_x_tr_sl=auto&_x_tr_tl=${userLang}&_x_tr_hl=en&_x_tr_pto=wapp"));
+                    },
+                    icon: SvgPicture.asset(
+                        "assets/ResourceCenter/icon-serna.svg"),
                     color: Colors.white,
                   ),
                   Spacer(),
-                ]
+                ]),
               ),
-            ),
-            Spacer(),
-          ],
-        ),
-        Spacer(),Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Spacer(),
-            Container(
-              width: 100,
-              height: 100,
-              decoration: BoxDecoration(
-                color: Colors.black,
-                border: Border.all(
-                  color: Color(0x0008231A)
+              Spacer(),
+            ],
+          ),
+          Spacer(),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Spacer(),
+              Container(
+                width: 100,
+                height: 100,
+                decoration: BoxDecoration(
+                  color: Colors.black,
+                  border: Border.all(color: Color(0x0008231A)),
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
                 ),
-                borderRadius: BorderRadius.all(Radius.circular(10)),
-              ),
-              child: Column(
-                children: [
+                child: Column(children: [
                   Text("APIDA", style: TextStyle(color: Colors.white)),
                   Text("Center", style: TextStyle(color: Colors.white)),
                   IconButton(
-                    onPressed: () {_launchURL(Uri.parse("https://www.csus.edu/student-affairs/centers-programs/asian-pacific-islander-desi-american-student-center/"));}, 
-                    icon: SvgPicture.asset("assets/ResourceCenter/icon-serna.svg"),
+                    onPressed: () {
+                      _launchURL(Uri.parse(
+                          "https://www-csus-edu.translate.goog/student-affairs/centers-programs/asian-pacific-islander-desi-american-student-center/?_x_tr_sl=auto&_x_tr_tl=${userLang}&_x_tr_hl=en&_x_tr_pto=wapp"));
+                    },
+                    icon: SvgPicture.asset(
+                        "assets/ResourceCenter/icon-serna.svg"),
                     color: Colors.white,
                   ),
-                ]
+                ]),
               ),
-            ),
-            Spacer(),
-            Container(
-              width: 100,
-              height: 100,
-              decoration: BoxDecoration(
-                color: Colors.black,
-                border: Border.all(
-                  color: Color(0x0008231A)
+              Spacer(),
+              Container(
+                width: 100,
+                height: 100,
+                decoration: BoxDecoration(
+                  color: Colors.black,
+                  border: Border.all(color: Color(0x0008231A)),
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
                 ),
-                borderRadius: BorderRadius.all(Radius.circular(10)),
-              ),
-              child: Column(
-                children: [
+                child: Column(children: [
                   Text("Health and", style: TextStyle(color: Colors.white)),
                   Text("Counseling", style: TextStyle(color: Colors.white)),
                   IconButton(
-                    onPressed: () {_launchURL(Uri.parse("https://www.csus.edu/student-life/health-counseling/"));}, 
-                    icon: SvgPicture.asset("assets/ResourceCenter/icon-heart.svg"),
+                    onPressed: () {
+                      _launchURL(Uri.parse(
+                          "https://www.csus.edu/student-life/health-counseling/"));
+                    },
+                    icon: SvgPicture.asset(
+                        "assets/ResourceCenter/icon-heart.svg"),
                     color: Colors.white,
                   ),
-                ]
+                ]),
               ),
-            ),
-            Spacer(),
-            Container(
-              width: 100,
-              height: 100,
-              decoration: BoxDecoration(
-                color: Colors.black,
-                border: Border.all(
-                  color: Color(0x0008231A)
+              Spacer(),
+              Container(
+                width: 100,
+                height: 100,
+                decoration: BoxDecoration(
+                  color: Colors.black,
+                  border: Border.all(color: Color(0x0008231A)),
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
                 ),
-                borderRadius: BorderRadius.all(Radius.circular(10)),
-              ),
-              child: Column(
-                children: [
+                child: Column(children: [
                   Text("Basic Needs", style: TextStyle(color: Colors.white)),
                   Spacer(),
                   IconButton(
-                    onPressed: () {_launchURL(Uri.parse("https://www.csus.edu/student-affairs/crisis-assistance-resource-education-support/resources.html"));}, 
-                    icon: SvgPicture.asset("assets/ResourceCenter/icon-basic.svg"),
+                    onPressed: () {
+                      _launchURL(Uri.parse(
+                          "https://www-csus-edu.translate.goog/student-life/health-counseling/?_x_tr_sl=auto&_x_tr_tl=${userLang}&_x_tr_hl=en&_x_tr_pto=wapp"));
+                    },
+                    icon: SvgPicture.asset(
+                        "assets/ResourceCenter/icon-basic.svg"),
                     color: Colors.white,
                   ),
                   Spacer(),
-                ]
+                ]),
               ),
-            ),
-            Spacer(),
-          ],
-        ),
-        Spacer(),
-      ]
-    );
+              Spacer(),
+            ],
+          ),
+          Spacer(),
+        ]);
   }
-  
 }
