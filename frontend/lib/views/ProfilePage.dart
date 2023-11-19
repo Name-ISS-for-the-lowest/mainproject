02dbd090f9e7dbc87f65027d8670ac1ef608fba4
Rylan Pereira <rylanrpcollege@gmail.com>
2023-11-18 16:05:19 -0800

    Implemented Language List List contents unverified

--- frontend/lib/views/ProfilePage.dart
+++ frontend/lib/views/ProfilePage.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:country_picker/country_picker.dart';
-import 'package:reactive_language_picker/reactive_language_picker.dart';
-import 'package:reactive_forms/reactive_forms.dart';
+import 'package:language_picker/language_picker.dart';
+import 'package:language_picker/languages.dart';
 import 'package:frontend/classes/authHelper.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter_cache_manager/flutter_cache_manager.dart';
@@ -13,15 +13,92 @@ class ProfilePage extends StatefulWidget {
   State<ProfilePage> createState() => _ProfilePageState();
 }
 
+
+
+
+
+
+
 class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
+    //USER VARIABLES
     String imageURL = AuthHelper.userInfoCache['profilePicture.url'];
     String displayName = AuthHelper.userInfoCache['username'];
     String nationality = AuthHelper.userInfoCache['nationality'];
     String language =
         AuthHelper.languageNames[AuthHelper.userInfoCache['language']];
     String emailAddress = AuthHelper.userInfoCache['email'];
+    Language _selectedDialogLanguage = Languages.english;
+
+
+//COUNTRY SELECTOR
+  void countryselect()
+  {
+    showCountryPicker(
+      context: context,
+      favorite: <String>['US', 'CN', 'MX', 'IN'],
+      //exclude: <String>['FR'],
+      countryListTheme: CountryListThemeData(
+        backgroundColor: Color(0xffece7d5),
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(0),
+          topRight: Radius.circular(0),
+        ),
+      ),
+      onSelect: (Country country) {
+        print('Select country: ${country.displayName}');
+      },
+    );
+  }
+
+  Widget _buildDialogItem(Language language){
+    return Row(
+      children: <Widget>[
+        SizedBox(
+          width: 0.0,
+        ),
+        Text("${language.name}"),
+        //Text("${language.name} ({$language.isoCode})"),
+      ],
+    );
+  }
+
+  //Language WHITELIST
+  final supportedLanguages = [
+    Languages.english,
+    Languages.korean,
+
+
+
+
+
+
+
+
+
+  ];
+
+//LANGUAGE SELECTOR
+  void _openLanguagePickerDialog() => showDialog(
+      context: context,
+      builder: (context) => Theme(
+          data: Theme.of(context).copyWith(primaryColor: Colors.pink),
+          child: LanguagePickerDialog(
+              //languages: supportedLanguages,
+              titlePadding: EdgeInsets.all(8.0),
+              //searchCursorColor: Colors.pinkAccent,
+              searchInputDecoration: InputDecoration(hintText: 'Search'),
+              isSearchable: true,
+              
+              title: Text('Select your language'),
+              onValuePicked: (Language language) => setState(() {
+                    _selectedDialogLanguage = language;
+                    print(_selectedDialogLanguage.name);
+                    print(_selectedDialogLanguage.isoCode);
+                  }),
+              itemBuilder: _buildDialogItem)),
+  );
 
     return Scaffold(
         backgroundColor: Color(0xffece7d5),
@@ -80,6 +157,9 @@ class _ProfilePageState extends State<ProfilePage> {
                 alignment: Alignment.centerRight,
                 icon: Icon(Icons.edit_note),
                 onPressed: (){
+                    
+
+
 
                 },
                 
@@ -96,21 +176,7 @@ class _ProfilePageState extends State<ProfilePage> {
                 trailing: IconButton(
                   icon: Icon(Icons.edit_note),
                   onPressed: () {
-                    showCountryPicker(
-                      context: context,
-                      favorite: <String>['US', 'CN', 'MX', 'IN'],
-                      //exclude: <String>['FR'],
-                      countryListTheme: CountryListThemeData(
-                        backgroundColor: Color(0xffece7d5),
-                        borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(0),
-                          topRight: Radius.circular(0),
-                        ),
-                      ),
-                      onSelect: (Country country) {
-                        print('Select country: ${country.displayName}');
-                      },
-                    );
+                    countryselect();
                   },
                 ),
               ),
@@ -124,7 +190,10 @@ class _ProfilePageState extends State<ProfilePage> {
                 subtitle: Text(language),
                 trailing: IconButton(
                   icon: Icon(Icons.edit_note),
-                  onPressed: () {},
+                  onPressed: () {
+                    //OPEN THE LANGUAGE SELECTOR
+                    _openLanguagePickerDialog();
+                  },
                 ),
               ),
             ),
