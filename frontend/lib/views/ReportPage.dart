435c998edc5dc0890f0d71e4a80e7e5ed48dde8e
tjudtmartine <145167310+tjudtmartine@users.noreply.github.com>
2023-11-22 16:12:47 -0800

    report page shell

--- frontend/lib/views/ReportPage.dart
+++ frontend/lib/views/ReportPage.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:frontend/views/CoreTemplate.dart';
 
@@ -11,6 +10,8 @@ class ReportPage extends StatefulWidget {
 }
 
 class _ReportPageState extends State<ReportPage> {
+  String selectedOption = '';
+
   void navigateToPrimaryScreens() {
     Navigator.of(context).push(
       MaterialPageRoute(
@@ -21,6 +22,10 @@ class _ReportPageState extends State<ReportPage> {
         },
       ),
     );
+  }
+
+  void submitReport() {
+    print("Selected Option: $selectedOption");
   }
 
   @override
@@ -40,7 +45,7 @@ class _ReportPageState extends State<ReportPage> {
             ),
             onTap: () => navigateToPrimaryScreens(),
           ),
-          title: Text(
+          title: const Text(
             "Report Post",
             textAlign: TextAlign.center,
             style: TextStyle(
@@ -55,24 +60,102 @@ class _ReportPageState extends State<ReportPage> {
               height: 1.0, // Set the desired height of the line
               color: const Color(0x5f000000),
             ),
-          )),
-      body: Align(
-        alignment: Alignment.center,
-        child: Container(
-          height: 800,
-          width: 350,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Text("Are you sure you want to report this post?"),
-              Text(
-                  "We at International Student Station take reports very seriously, and highly encourage our users to report posts when they feel as if our rules are broken."),
-              Text(
-                  "Please select the rule(s) you felt were violated in the post, and hit submit if you would like to file a report. Alternatively, hit the back arrow on the top left of your screen to cancel the report."),
-            ],
-          ),
+          )
         ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+            padding: const EdgeInsets.all(20.0),
+            child:  const Column(
+              children: [
+                Text(
+                  "Are you sure you want to report this post?",
+                  style: TextStyle(
+                  fontSize: 16.0,
+                  fontWeight: FontWeight.bold,),
+                  ),
+                SizedBox(height: 10.0),
+                Text(
+                  "We at International Student Station take reports very seriously, and highly encourage our users to report posts when they feel as if our rules are broken.",
+                ),
+                SizedBox(height: 10.0),
+                Text(
+                  "Please select the rule(s) you felt were violated in the post, and hit submit if you would like to file a report. Alternatively, hit the back arrow on the top left of your screen to cancel the report.",
+                ),
+              ]
+            )
+          ),
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 110.0),
+            child:  Column(
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      selectedOption = 'Hate Speech';
+                    });
+                  },
+                  child: buildMenu('Hate Speech'),
+                ),
+                GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      selectedOption = 'Illegal Content';
+                    });
+                  },
+                  child: buildMenu('Illegal Content'),
+                ),
+                GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      selectedOption = 'Targeted Harassment';
+                    });
+                  },
+                  child: buildMenu('Targeted Harassment'),
+                ),
+                GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      selectedOption = 'Inappropriate Content';
+                    });
+                  },
+                  child: buildMenu('Inappropriate Content'),
+                ),
+                GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      selectedOption = 'Other Reason';
+                    });
+                  },
+                  child: buildMenu('Other Reason'),
+                ),
+              ]
+            )
+          ),
+          SizedBox(height: 16.0),
+          ElevatedButton(
+            onPressed: submitReport,
+            child: Text("Submit"),
+          ),
+        ],
+      ),
+    );
+  }
+  Widget buildMenu(String option) {
+    return Container(
+      padding: const EdgeInsets.symmetric(vertical: 10.0),
+      child: Row(
+        children: [
+          Text(
+            option,
+            style: TextStyle(
+              fontSize: 16.0,
+              fontWeight: FontWeight.bold,
+              color: selectedOption == option ? Colors.blue : Colors.black,
+            ),
+          ),
+        ],
       ),
     );
   }
