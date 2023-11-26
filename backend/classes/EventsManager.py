bd6493a0d7927dd0465d0aeb6669f45d4a580d5b
Gabriel Malek <malek.gabriel33@gmail.com>
2023-11-25 18:10:28 -0800

    event translations

--- backend/classes/EventsManager.py
+++ backend/classes/EventsManager.py
@@ -14,7 +14,47 @@ class EventsManager:
         eventObjects = []
         for event in events:
             eventObjects.append(Event.fromDict(event))
+        return eventObjects
 
     @staticmethod
     def translateEvents(lang: str, events: list[Event]):
-        print("translateEvents")
+        if lang == "en":
+            return Event.listToDics(events)
+        listOfStrings = []
+        for event in events:
+            listOfStrings.append(event.title["en"])
+            listOfStrings.append(event.date["en"])
+            listOfStrings.append(event.description["en"])
+        for i, item in enumerate(listOfStrings):
+            # print("|", item, "|")
+            if item == "":
+                # print("empty string")
+                listOfStrings[i] = "NONE"
+        # print(listOfStrings)
+
+        jsonData = json.dumps(
+            {
+                "stringList": listOfStrings,
+            }
+        )
+        try:
+            results = requests.post(
+                "https://issapp.gabrielmalek.com/bulkTranslate?target=" + lang,
+                data=jsonData,
+                headers={
+                    "Content-Type": "application/json",
+                },
+            )
+            finalList = results.json()["result"]
+            for event in events:
+                event.title[lang] = finalList.pop(0)
+                event.date[lang] = finalList.pop(0)
+                event.description[lang] = finalList.pop(0)
+
+            # save the events to json file
+            jsonEvents = Event.listToDics(events)
+            return jsonEvents
+            # break
+        except Exception as e:
+            print("failed on ", e)
+            exit()
